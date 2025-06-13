5db299de14f9f7dc98aac34983ccdf07c45d81eb
Marlene Silva Marchena <13339773+msmarchena@users.noreply.github.com>
2025-06-13 10:42:07 +0200

    Update jokes.py

--- Getting_started_with_python/jokes.py
+++ Getting_started_with_python/jokes.py
@@ -1,19 +1,1 @@
-import pyjokes
-from deep_translator import GoogleTranslator
 
-# Here you have Jokes translate to French
-    
-def translate_to_french(text):
-    translator = GoogleTranslator(source="en", target="fr")
-    translated = translator.translate(text)
-    return translated
-
-def tell_french_joke():
-    joke = pyjokes.get_joke()
-    french_joke = translate_to_french(joke)
-    print("Blague en français :")
-    print(french_joke)
-    
-    
-if __name__ == "__main__":
-    tell_french_joke()
